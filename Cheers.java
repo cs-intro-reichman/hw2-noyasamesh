211cb2a3264029b2f3ac07aaaf96209e65dd351f
Noya Samesh <noya.samesh@post.runi.ac.il>
2025-11-14 00:12:46 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -1,21 +1,26 @@
 
 public class Cheers {
-        public static void main(String[] args) {
-                String name = args[0];
-                name = name.toUpperCase();
-                int num = Integer.parseInt(args[1]);
-                String an = "AEFHILMNORSX";
-                for(int i=0; i < name.length(); i++){
-                        char c = name.charAt(i);
-                        if(an.indexOf(c) == -1)
-                        System.out.println("Give me a  " + name.charAt(i) + ": " + name.charAt(i) + "!");
-                        else 
-                        System.out.println("Give me aמ  " + name.charAt(i) + ": " + name.charAt(i) + "!");
-                }
-                System.out.println("What does that spell?");
-                for( int i=1; i <= num; i++){
-                        System.out.println(name + "!!!");
-                }
+    public static void main(String[] args) {
+        String name = args[0].toUpperCase(); // כל האותיות גדולות
+        int num = Integer.parseInt(args[1]);
+        String an = "AEFHILMNORSX"; // אותיות שמקבלות "an"
 
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (an.indexOf(c) != -1) {
+                System.out.println("Give me an " + c + ": " + c + "!");
+            } else {
+                System.out.println("Give me a " + c + ": " + c + "!");
+            }
         }
+
+        System.out.println("What does that spell?");
+        for (int i = 1; i <= num; i++) {
+            System.out.println(name + "!!!");
+        }
+    }
 }
+
+
+        
+
