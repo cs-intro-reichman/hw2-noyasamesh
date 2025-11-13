5401415903c28d565f02975014b0b1fb13437bd2
Noya Samesh <noya.samesh@post.runi.ac.il>
2025-11-14 00:15:15 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -1,16 +1,16 @@
 
 public class Cheers {
     public static void main(String[] args) {
-        String name = args[0].toUpperCase(); // כל האותיות גדולות
+        String name = args[0].toUpperCase();
         int num = Integer.parseInt(args[1]);
-        String an = "AEFHILMNORSX"; // אותיות שמקבלות "an"
+        String an = "AEFHILMNORSX";
 
         for (int i = 0; i < name.length(); i++) {
             char c = name.charAt(i);
             if (an.indexOf(c) != -1) {
                 System.out.println("Give me an " + c + ": " + c + "!");
             } else {
-                System.out.println("Give me a " + c + ": " + c + "!");
+                System.out.println("Give me a  " + c + ": " + c + "!"); // שני רווחים אחרי a
             }
         }
 
@@ -20,6 +20,8 @@ public class Cheers {
         }
     }
 }
+
+    
 
 
         
